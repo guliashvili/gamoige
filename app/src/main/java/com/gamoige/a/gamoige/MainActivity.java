70f6061a790a53ed1ef787bb39e14d97fed9c350
Giorgi Guliashvili <gguli13@freeuni.edu.ge>
2017-07-14 17:54:25 +0400

    leaderboard rom creation

--- app/src/main/java/com/gamoige/a/gamoige/MainActivity.java
+++ app/src/main/java/com/gamoige/a/gamoige/MainActivity.java
@@ -1,23 +1,48 @@
 package com.gamoige.a.gamoige;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.drive.Drive;
 import com.google.android.gms.games.Games;
+import com.google.android.gms.games.GamesStatusCodes;
+import com.google.android.gms.games.multiplayer.Invitation;
+import com.google.android.gms.games.multiplayer.Multiplayer;
+import com.google.android.gms.games.multiplayer.Participant;
+import com.google.android.gms.games.multiplayer.realtime.RealTimeMessage;
+import com.google.android.gms.games.multiplayer.realtime.RealTimeMessageReceivedListener;
+import com.google.android.gms.games.multiplayer.realtime.Room;
+import com.google.android.gms.games.multiplayer.realtime.RoomConfig;
+import com.google.android.gms.games.multiplayer.realtime.RoomStatusUpdateListener;
+import com.google.android.gms.games.multiplayer.realtime.RoomUpdateListener;
 import com.google.example.games.basegameutils.BaseGameUtils;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, RoomUpdateListener, RealTimeMessageReceivedListener, RoomStatusUpdateListener {
+    private static final int REQUEST_LEADERBOARD = 1;
     public static GoogleApiClient mGoogleApiClient;
     private static int RC_SIGN_IN = 9001;
+    // request code for the "select players" UI
+    // can be any number as long as it's unique
+    final static int RC_SELECT_PLAYERS = 10000;
+    // at least 2 players required for our game
+    final static int MIN_PLAYERS = 2;
 
+
+    // are we already playing?
+    boolean mPlaying = false;
     private boolean mResolvingConnectionFailure = false;
     private boolean mAutoStartSignInFlow = true;
     private boolean mSignInClicked = false;
@@ -81,14 +106,39 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         super.onStop();
         mGoogleApiClient.disconnect();
     }
+
     @Override
     public void onConnected(Bundle connectionHint) {
+        //TODO
         // The player is signed in. Hide the sign-in button and allow the
         // player to proceed.
+
+
+        if (connectionHint != null) {
+            Invitation inv =
+                    connectionHint.getParcelable(Multiplayer.EXTRA_INVITATION);
+
+            if (inv != null) {
+                // accept invitation
+                RoomConfig.Builder roomConfigBuilder = makeBasicRoomConfigBuilder();
+                roomConfigBuilder.setInvitationIdToAccept(inv.getInvitationId());
+                Games.RealTimeMultiplayer.join(mGoogleApiClient, roomConfigBuilder.build());
+
+                // prevent screen from sleeping during handshake
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+                // go to game screen
+            }
+        }
+
+
     }
 
     protected void onActivityResult(int requestCode, int resultCode,
                                     Intent intent) {
+        Intent data = intent;
+        int request = requestCode;
+        int response = resultCode;
         if (requestCode == RC_SIGN_IN) {
             mSignInClicked = false;
             mResolvingConnectionFailure = false;
@@ -102,6 +152,42 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 BaseGameUtils.showActivityResultError(this,
                         requestCode, resultCode, R.string.signin_failure);
             }
+        } else if (request == RC_SELECT_PLAYERS) {
+            if (response != Activity.RESULT_OK) {
+                // user canceled
+                return;
+            }
+
+            // get the invitee list
+            Bundle extras = data.getExtras();
+            final ArrayList<String> invitees =
+                    data.getStringArrayListExtra(Games.EXTRA_PLAYER_IDS);
+
+            // get auto-match criteria
+            Bundle autoMatchCriteria = null;
+            int minAutoMatchPlayers =
+                    data.getIntExtra(Multiplayer.EXTRA_MIN_AUTOMATCH_PLAYERS, 0);
+            int maxAutoMatchPlayers =
+                    data.getIntExtra(Multiplayer.EXTRA_MAX_AUTOMATCH_PLAYERS, 0);
+
+            if (minAutoMatchPlayers > 0) {
+                autoMatchCriteria = RoomConfig.createAutoMatchCriteria(
+                        minAutoMatchPlayers, maxAutoMatchPlayers, 0);
+            } else {
+                autoMatchCriteria = null;
+            }
+
+            // create the room and specify a variant if appropriate
+            RoomConfig.Builder roomConfigBuilder = makeBasicRoomConfigBuilder();
+            roomConfigBuilder.addPlayersToInvite(invitees);
+            if (autoMatchCriteria != null) {
+                roomConfigBuilder.setAutoMatchCriteria(autoMatchCriteria);
+            }
+            RoomConfig roomConfig = roomConfigBuilder.build();
+            Games.RealTimeMultiplayer.create(mGoogleApiClient, roomConfig);
+
+            // prevent screen from sleeping during handshake
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         }
     }
 
@@ -116,5 +202,194 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         mSignInClicked = false;
         Games.signOut(mGoogleApiClient);
     }
+
+
+    /****************************************/
+    //matchmaking
+
+    /*******************************************/
+
+
+    public void startLeaderBoard(View view) {
+        //TODO remove
+        Games.Leaderboards.submitScore(mGoogleApiClient, getString(R.string.LEADERBOARD_ID), 1337);
+
+
+        startActivityForResult(Games.Leaderboards.getLeaderboardIntent(mGoogleApiClient,
+                getString(R.string.LEADERBOARD_ID)), REQUEST_LEADERBOARD);
+    }
+
+    @Override
+    public void onRoomConnecting(Room room) {
+
+    }
+
+    @Override
+    public void onRoomAutoMatching(Room room) {
+
+    }
+
+    @Override
+    public void onPeerInvitedToRoom(Room room, List<String> list) {
+
+    }
+
+    @Override
+    public void onPeerDeclined(Room room, List<String> peers) {
+        // peer declined invitation -- see if game should be canceled
+        if (!mPlaying && shouldCancelGame(room)) {
+            Games.RealTimeMultiplayer.leave(mGoogleApiClient, null, room.getRoomId());
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+    }
+
+    @Override
+    public void onPeerJoined(Room room, List<String> list) {
+
+    }
+
+    @Override
+    public void onPeerLeft(Room room, List<String> peers) {
+        // peer left -- see if game should be canceled
+        if (!mPlaying && shouldCancelGame(room)) {
+            Games.RealTimeMultiplayer.leave(mGoogleApiClient, null, room.getRoomId());
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+    }
+
+    @Override
+    public void onConnectedToRoom(Room room) {
+
+    }
+
+    @Override
+    public void onDisconnectedFromRoom(Room room) {
+
+    }
+
+    @Override
+    public void onPeersConnected(Room room, List<String> peers) {
+        if (mPlaying) {
+            // add new player to an ongoing game
+        } else if (shouldStartGame(room)) {
+            // start game!
+        }
+    }
+
+    @Override
+    public void onPeersDisconnected(Room room, List<String> peers) {
+        if (mPlaying) {
+            // do game-specific handling of this -- remove player's avatar
+            // from the screen, etc. If not enough players are left for
+            // the game to go on, end the game and leave the room.
+        } else if (shouldCancelGame(room)) {
+            // cancel the game
+            Games.RealTimeMultiplayer.leave(mGoogleApiClient, null, room.getRoomId());
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+    }
+
+    @Override
+    public void onP2PConnected(String s) {
+
+    }
+
+    @Override
+    public void onP2PDisconnected(String s) {
+
+    }
+
+    @Override
+    public void onRealTimeMessageReceived(RealTimeMessage realTimeMessage) {
+
+    }
+
+    @Override
+    public void onRoomCreated(int statusCode, Room room) {
+        if (statusCode != GamesStatusCodes.STATUS_OK) {
+            // let screen go to sleep
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+            // show error message, return to main screen.
+        }
+    }
+
+    @Override
+    public void onJoinedRoom(int statusCode, Room room) {
+        if (statusCode != GamesStatusCodes.STATUS_OK) {
+            // let screen go to sleep
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+            // show error message, return to main screen.
+        }
+    }
+
+    @Override
+    public void onLeftRoom(int i, String s) {
+
+    }
+
+    @Override
+    public void onRoomConnected(int statusCode, Room room) {
+        if (statusCode != GamesStatusCodes.STATUS_OK) {
+            // let screen go to sleep
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+            // show error message, return to main screen.
+        }
+    }
+
+    private RoomConfig.Builder makeBasicRoomConfigBuilder() {
+        return RoomConfig.builder(this)
+                .setMessageReceivedListener(this)
+                .setRoomStatusUpdateListener(this);
+    }
+
+
+    public void startQuickGame(View view) {
+        // auto-match criteria to invite one random automatch opponent.
+        // You can also specify more opponents (up to 3).
+        Bundle am = RoomConfig.createAutoMatchCriteria(1, 1, 0);
+
+        // build the room config:
+        RoomConfig.Builder roomConfigBuilder = makeBasicRoomConfigBuilder();
+        roomConfigBuilder.setAutoMatchCriteria(am);
+        RoomConfig roomConfig = roomConfigBuilder.build();
+
+        // create room:
+        Games.RealTimeMultiplayer.create(mGoogleApiClient, roomConfig);
+
+        // prevent screen from sleeping during handshake
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        // go to game screen
+        //TODO
+    }
+
+    public void startSelectFriends(View view) {
+        // launch the player selection screen
+        // minimum: 1 other player; maximum: 3 other players
+        Intent intent = Games.RealTimeMultiplayer.getSelectOpponentsIntent(mGoogleApiClient, 1, 3);
+        startActivityForResult(intent, RC_SELECT_PLAYERS);
+    }
+
+    // returns whether there are enough players to start the game
+    boolean shouldStartGame(Room room) {
+        int connectedPlayers = 0;
+        for (Participant p : room.getParticipants()) {
+            if (p.isConnectedToRoom()) ++connectedPlayers;
+        }
+        return connectedPlayers >= MIN_PLAYERS;
+    }
+
+    // Returns whether the room is in a state where the game should be canceled.
+    boolean shouldCancelGame(Room room) {
+        // TODO: Your game-specific cancellation logic here. For example, you might decide to
+        // cancel the game if enough people have declined the invitation or left the room.
+        // You can check a participant's status with Participant.getStatus().
+        // (Also, your UI should have a Cancel button that cancels the game too)
+        return false;
+    }
+
 
 }
