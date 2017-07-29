579e7c3107ab06a19fe44e62a3b7f610bb4b2ac0
BekaMaisuradze <fcb10lal24@gmail.com>
2017-07-29 13:19:00 +0400

    Add waiting gif animation. Freeze actions while loading

--- app/src/main/java/com/gamoige/a/gamoige/Fragments/HomeScreen.java
+++ app/src/main/java/com/gamoige/a/gamoige/Fragments/HomeScreen.java
@@ -14,6 +14,7 @@ import com.gamoige.a.gamoige.PreviewActivity;
 import com.gamoige.a.gamoige.R;
 import com.google.android.gms.games.Games;
 import com.google.android.gms.games.multiplayer.realtime.RoomConfig;
+import com.wang.avi.AVLoadingIndicatorView;
 
 /**
  * Created by Donsky on 7/17/2017.
@@ -21,28 +22,38 @@ import com.google.android.gms.games.multiplayer.realtime.RoomConfig;
 
 public class HomeScreen extends Fragment {
     private ConnectionFragment connectionFragment;
+    private AVLoadingIndicatorView avi;
     private View view;
+    private boolean loading;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.activity_main, container, false);
+        avi = (AVLoadingIndicatorView) view.findViewById(R.id.avi);
+        loading = false;
         view.findViewById(R.id.leaderBoardBtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                avi.show();
                 startLeaderBoard(v);
+                loading = true;
             }
         });
         view.findViewById(R.id.quickGameBtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                avi.show();
                 startQuickGame(v);
+                loading = true;
             }
         });
         view.findViewById(R.id.selectFriendBtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                avi.show();
                 startSelectFriends(v);
+                loading = true;
             }
         });
         return view;
@@ -55,44 +66,59 @@ public class HomeScreen extends Fragment {
 
     public void startLeaderBoard(View view) {
         Log.e("info","startLeaderBoard");
-        //TODO remove
-        Games.Leaderboards.submitScore(connectionFragment.getConnection(), getString(R.string.LEADERBOARD_ID), 1337);
+
+        if (!loading) {
+            //TODO remove
+            Games.Leaderboards.submitScore(connectionFragment.getConnection(), getString(R.string.LEADERBOARD_ID), 1337);
 
 
-        startActivityForResult(Games.Leaderboards.getLeaderboardIntent(connectionFragment.getConnection(),
-                getString(R.string.LEADERBOARD_ID)), ConnectionFragment.REQUEST_LEADERBOARD);
+            startActivityForResult(Games.Leaderboards.getLeaderboardIntent(connectionFragment.getConnection(),
+                    getString(R.string.LEADERBOARD_ID)), ConnectionFragment.REQUEST_LEADERBOARD);
+        }
     }
 
     public void startQuickGame(View view) {
         Log.e("info","startQuickGame");
-        // auto-match criteria to invite one random automatch opponent.
-        // You can also specify more opponents (up to 3).
-        Bundle am = RoomConfig.createAutoMatchCriteria(1, 3, 0);
 
-        // build the room config:
-        RoomConfig.Builder roomConfigBuilder = connectionFragment.makeBasicRoomConfigBuilder();
-        roomConfigBuilder.setAutoMatchCriteria(am);
-        RoomConfig roomConfig = roomConfigBuilder.build();
+        if (!loading) {
+            // auto-match criteria to invite one random automatch opponent.
+            // You can also specify more opponents (up to 3).
+            Bundle am = RoomConfig.createAutoMatchCriteria(1, 3, 0);
 
-        // create room:
-        Games.RealTimeMultiplayer.create(connectionFragment.getConnection(), roomConfig);
+            // build the room config:
+            RoomConfig.Builder roomConfigBuilder = connectionFragment.makeBasicRoomConfigBuilder();
+            roomConfigBuilder.setAutoMatchCriteria(am);
+            RoomConfig roomConfig = roomConfigBuilder.build();
 
-        // prevent screen from sleeping during handshake
-        getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            // create room:
+            Games.RealTimeMultiplayer.create(connectionFragment.getConnection(), roomConfig);
 
-        // go to game screen
-        //TODO
+            // prevent screen from sleeping during handshake
+            getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+            // go to game screen
+            //TODO
+        }
     }
 
     public void startSelectFriends(View view) {
         Log.e("info","startSelectFriends");
-        // launch the player selection screen
-        // minimum: 1 other player; maximum: 3 other players
-        Intent intent = Games.RealTimeMultiplayer.getSelectOpponentsIntent(connectionFragment.getConnection(), 1, 3);
-        startActivityForResult(intent, ConnectionFragment.RC_SELECT_PLAYERS);
+        if (!loading) {
+            // launch the player selection screen
+            // minimum: 1 other player; maximum: 3 other players
+            Intent intent = Games.RealTimeMultiplayer.getSelectOpponentsIntent(connectionFragment.getConnection(), 1, 3);
+            startActivityForResult(intent, ConnectionFragment.RC_SELECT_PLAYERS);
+        }
     }
 
     public void setActive(boolean active){
         view.setVisibility(active ? View.VISIBLE : View.GONE);
+    }
+
+    @Override
+    public void onResume() {
+        avi.hide();
+        loading = false;
+        super.onResume();
     }
 }
