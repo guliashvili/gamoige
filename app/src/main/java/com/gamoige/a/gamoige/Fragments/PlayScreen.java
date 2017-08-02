5a25f1f2b2a8ca434054080618131023ca31937a
TheDonsky <nikadon.net@gmail.com>
2017-08-02 22:00:24 +0400

    Add some verbosity

--- app/src/main/java/com/gamoige/a/gamoige/Fragments/PlayScreen.java
+++ app/src/main/java/com/gamoige/a/gamoige/Fragments/PlayScreen.java
@@ -142,7 +142,7 @@ public class PlayScreen extends Fragment implements CanvasListener{
                             .setIcon(R.drawable.player_won)
                             .setTitle(R.string.game_result)
                             .setMessage(connectionFragment.getRoom().getParticipant(guessMessage.getSender())
-                                    .getDisplayName() + getString(R.string.someone_won))
+                                    .getDisplayName() + " " + getString(R.string.someone_won))
                             .show();
                     Log.e("givorgi", "won: " + guessMessage.getSender() + " " + guessMessage.getMsg());
                     ((MainActivity) getActivity()).set(MainActivity.Mode.HOME_SCREEN);
--- app/src/main/java/com/gamoige/a/gamoige/packages/YleMessage.java
+++ app/src/main/java/com/gamoige/a/gamoige/packages/YleMessage.java
@@ -3,6 +3,8 @@ package com.gamoige.a.gamoige.packages;
 import android.util.Log;
 
 import com.gamoige.a.gamoige.Fragments.ConnectionFragment;
+import com.gamoige.a.gamoige.R;
+import com.yarolegovich.lovelydialog.LovelyInfoDialog;
 
 /**
  * Created by a on 7/31/17.
@@ -18,5 +20,11 @@ public class YleMessage implements Package{
     @Override
     public void doit(ConnectionFragment fragment, String senderId) {
         Log.e("givorgi", "gaajvi message");
+        new LovelyInfoDialog(fragment.getContext())
+                .setTopColorRes(R.color.gameResultDialogColor)
+                .setIcon(R.drawable.player_lost)
+                .setTitle(R.string.wrong_answer)
+                .setMessage(msg)
+                .show();
     }
 }
